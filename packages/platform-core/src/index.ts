227f721b05b4b6da660074e4d54936f69283eeba
heke99 <129280077+heke99@users.noreply.github.com>
2026-08-24 15:03:16 +0200

    feat(platform): export portability contracts

--- packages/platform-core/src/index.ts
+++ packages/platform-core/src/index.ts
@@ -3,5 +3,6 @@ export * from "./credentials";
 export * from "./evals";
 export * from "./knowledge";
 export * from "./policy";
+export * from "./portability";
 export * from "./sandbox";
 export * from "./tools";
